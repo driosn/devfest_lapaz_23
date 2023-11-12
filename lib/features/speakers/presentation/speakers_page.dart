9ff76058401cb41579704d14fd28adfde132f3de
David Rios <davidsamuelrios07@gmail.com>
2023-11-12 00:05:46 -0400

    Added speakers page

---
+++ lib/features/speakers/presentation/speakers_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SpeakersPage extends StatelessWidget {
+  const SpeakersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Speakers Page'),
+      ),
+    );
+  }
+}
