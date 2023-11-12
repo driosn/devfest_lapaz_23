bcdf315e2f3c802d668061074295d359b2ca6dd0
David Rios <davidsamuelrios07@gmail.com>
2023-11-11 23:02:24 -0400

    Initial project setup

---
+++ lib/shared/style/devfest23_colors.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+class DevFest23Colors {
+  //
+  // Primary
+  //
+  static const blue = Color(0xff4285f4);
+  static const green = Color(0xff34a853);
+  static const yellow = Color(0xfff9ab00);
+  static const red = Color(0xffea4335);
+
+  //
+  // Light
+  //
+  static const blueLight = Color(0xff8ab4f8);
+  static const greenLight = Color(0xff81c995);
+  static const yellowLight = Color(0xfffde293);
+  static const redLight = Color(0xfff28b82);
+
+  //
+  // Neutrals
+  //
+  static const neutralBlack = Color(0xff000000);
+  static const neutralGrey = Color(0xff5f6368);
+  static const neutralGreyLight = Color(0xfff1f3f4);
+  static const neutralWhite = Color(0xffFFFFFF);
+
+  //
+  // Exceptions
+  //
+  static const exceptionYellow900 = Color(0xffea8600);
+  static const exceptionYellow500 = Color(0xfffbbc04);
+}
